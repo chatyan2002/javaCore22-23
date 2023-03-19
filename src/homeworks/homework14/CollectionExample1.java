3f464b1254c4445bb69dc8d32c6436dd00072f79
Eduard <chatyan27@gmail.com>
2023-03-19 15:49:57 +0400

    homework15

--- src/homeworks/homework14/CollectionExample1.java
+++ src/homeworks/homework14/CollectionExample1.java
@@ -16,26 +16,37 @@ public class CollectionExample1 {
         names.add("moso");
 
 
-        //public static void checkNames() {
-            System.out.println("please input name");
-            String name = scanner.nextLine();
-            if (names.contains(name)) {
-                System.out.println("this name already exists!");
-            } else {
-                System.out.println("there is no name!");
-            }
+        searchName(names);
 
-        System.out.println("please input name");
-        String Name = scanner.nextLine();
-        System.out.println(names.remove(Name));
-        System.out.println(names);
+        deleteName(names);
 
+        changeName(names);
+
+
+    }
+
+    private static void changeName(ArrayList<String> names) {
         String in = scanner.nextLine();
         String nam = scanner.nextLine();
         names.add(Integer.parseInt(in),nam);
         System.out.println(names);
+    }
 
+    private static void deleteName(ArrayList<String> names) {
+        System.out.println("please input name");
+        String Name = scanner.nextLine();
+        System.out.println(names.remove(Name));
+        System.out.println(names);
+    }
 
+    private static void searchName(ArrayList<String> names) {
+        System.out.println("please input name");
+        String name = scanner.nextLine();
+        if (names.contains(name)) {
+            System.out.println("this name already exists!");
+        } else {
+            System.out.println("there is no name!");
+        }
     }
 
 
--- src/homeworks/homework15/FileUtil.java
+++ src/homeworks/homework15/FileUtil.java
@@ -1,27 +1,100 @@
 package homeworks.homework15;
 
-import java.io.File;
+import java.io.*;
 import java.util.Scanner;
 
 public class FileUtil {
 
     private static Scanner scanner = new Scanner(System.in);
 
-    public static void main(String[] args) {
-        fileSearch();
+    public static void main(String[] args) throws IOException {
+//        fileSearch();
+//        contentSearch();
+//        findLines();
+//        printSizeOfPackage();
+//        createFileWithContent();
+
     }
 
     static void fileSearch() {
-        File file = new File("C:\\Users\\User\\IdeaProjects\\homework\\src\\homeworks\\homework15");
         System.out.println("Please input pathName");
         String pathName = scanner.nextLine();
         System.out.println("Please input fileName");
         String fileName = scanner.nextLine();
-        if (pathName.equals(file)) {
-            System.out.println(true);
-        } else {
-            System.out.println(false);
+        File file = new File(pathName, fileName);
+        System.out.println(file.isFile());
+    }
+
+    static void contentSearch() throws IOException {
+        System.out.println("Please input pathName");
+        String pathName1 = scanner.nextLine();
+        System.out.println("Please input text");
+        String keyword = scanner.nextLine();
+        File directory = new File(pathName1);
+
+        File[] files = directory.listFiles();
+        for (File file : files) {
+            BufferedReader reader = new BufferedReader(new FileReader(file));
+            String line = reader.readLine();
+            while (line != null) {
+                if (line.contains(keyword)) {
+                    System.out.println("File path " + file.getAbsolutePath());
+                    break;
+                }
+                reader.close();
+            }
         }
 
+    }
+
+    static void findLines() {
+        System.out.println("Please input txtPath.txt");
+        String path = scanner.nextLine();
+        System.out.println("Please input keyword");
+        String keyword = scanner.nextLine();
+
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(path))) {
+            bw.write(keyword);
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    static void printSizeOfPackage() {
+        System.out.println("Please input path");
+        String pathName = scanner.nextLine();
+        File directory = new File(pathName);
+        long totalLenght = 0;
+
+        if(directory.isDirectory()) {
+            File[] files = directory.listFiles();
+            for (File file : files) {
+                if(file.isDirectory()){
+                    totalLenght += file.length();
+                }
+            }
+        }
+        System.out.println("Total length of all files in the directory " + totalLenght + " bytes ");
+    }
+
+    static void createFileWithContent(){
+        System.out.println("Please input PathName");
+        String pathName = scanner.nextLine();
+        System.out.println("Please input fileName");
+        String fileName = scanner.nextLine();
+        System.out.println("Please input content");
+        String content = scanner.nextLine();
+        scanner.close();
+
+        try{
+            File file = new File(pathName, fileName);
+            FileWriter writer = new FileWriter(file);
+            writer.write(content);
+            writer.close();
+            System.out.println("Data was successfully written to the file");
+        } catch (IOException e){
+            System.out.println("An error occurred while writing data to file.");
+            e.printStackTrace();
+        }
     }
 }
